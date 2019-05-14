5e3c2f6d3497c97bf413f67f84c6773cae5a6e06
Dan Crosta <dcrosta@late.am>
2019-05-14 09:50:20 -0400

    add test for escape_env_var

--- test_registry.py
+++ test_registry.py
@@ -1,24 +1,18 @@
 import os
 import unittest
 
-try:
-    from urllib.request import urlopen
-except ImportError:
-    from urllib2 import urlopen
+from tox_docker import escape_env_var
 
 
 class ToxDockerRegistryTest(unittest.TestCase):
-    def test_it_sets_specific_env_vars(self):
-        self.assertEqual("env-var-value", os.environ["ENV_VAR"])
 
     def test_it_sets_automatic_env_vars(self):
         # the nginx image we use exposes port 80
         self.assertIn("DOCKER_IO_LIBRARY_NGINX_HOST", os.environ)
         self.assertIn("DOCKER_IO_LIBRARY_NGINX_80_TCP", os.environ)
 
-    def test_it_exposes_the_port(self):
-        # the nginx image we use exposes port 80
-        url = "http://{host}:{port}/".format(host=os.environ["DOCKER_IO_LIBRARY_NGINX_HOST"], port=os.environ["DOCKER_IO_LIBRARY_NGINX_80_TCP"])
-        response = urlopen(url)
-        self.assertEqual(200, response.getcode())
-        self.assertIn("Thank you for using nginx.", str(response.read()))
+    def test_escape_env_var(self):
+        self.assertEqual(
+            escape_env_var("my.private.registry/cat/image"),
+            "MY_PRIVATE_REGISTRY_CAT_IMAGE",
+        )
