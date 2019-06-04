8c13339b7dd9ae264d0cd3a89f872a962ebbe8f0
Stephen Finucane <stephen@that.guru>
2019-06-04 15:23:21 +0100

    Add initial sphinx configuration
    At the moment this is just including the README but we can flesh this
    out in the future.
    
    Signed-off-by: Stephen Finucane <stephen@that.guru>
    Closes: #31

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# http://www.sphinx-doc.org/en/master/config
+
+import os
+
+import vcversioner
+
+
+ROOT_DIR = os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'tox-docker'
+copyright = '2019, Dan Crosta'
+author = 'Dan Crosta'
+version = release = vcversioner.find_version(root=ROOT_DIR).version
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+]
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
