4a778ebe9472a095f4e24a119add8522e5c1a40b
Dan Crosta <dcrosta@late.am>
2020-04-19 16:38:18 -0400

    rqeuire that port keys are present; indentation

--- test_ports.py
+++ test_ports.py
@@ -8,11 +8,11 @@ class ToxDockerPortTest(unittest.TestCase):
     def test_it_exposes_only_specified_port(self):
         client = docker.from_env(version="auto")
         mysql_container = None
-        for container in client.containers.list()
-          if "mysql" in containers.attrs["Config"]["Image"]:
-              mysql_container = container
-              break
+        for container in client.containers.list():
+            if "mysql" in container.attrs["Config"]["Image"]:
+                mysql_container = container
+                break
 
         self.assertIsNotNone(mysql_container, "could not find mysql container")
-        self.assertIsNotNone(mysql_container.attrs["NetworkSettings"]["Ports"].get("3306/tcp"))
-        self.assertIsNone(mysql_container.attrs["NetworkSettings"]["Ports"].get("33060/tcp"))
+        self.assertIsNotNone(mysql_container.attrs["NetworkSettings"]["Ports"]["3306/tcp"])
+        self.assertIsNone(mysql_container.attrs["NetworkSettings"]["Ports"]["33060/tcp"])
