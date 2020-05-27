bc5b33fbfd7f868d909750f114b3cb9fe3cb9b19
Gordon Pendleton <wgordonw1@gmail.com>
2020-05-27 17:20:21 -0400

    renamed alpine vars to postgres

--- test_links.py
+++ test_links.py
@@ -8,12 +8,12 @@ class ToxDockerLinksTest(unittest.TestCase):
 
     def test_links_created(self):
         client = docker.from_env(version="auto")
-        alpine_container = None
+        postgres_container = None
         memcached_container = None
         docker_container = None
         for container in client.containers.list():
-            if alpine_container is None and "postgres" in container.attrs['Config']['Image']:
-                alpine_container = container
+            if postgres_container is None and "postgres" in container.attrs['Config']['Image']:
+                postgres_container = container
             
             if memcached_container is None and "memcached" in container.attrs['Config']['Image']:
                 memcached_container = container
@@ -21,30 +21,30 @@ class ToxDockerLinksTest(unittest.TestCase):
             if docker_container is None and "elasticsearch" in container.attrs['Config']['Image']:
                 docker_container = container
             
-            if all([alpine_container, memcached_container, docker_container]):
+            if all([postgres_container, memcached_container, docker_container]):
                 break
 
-        self.assertIsNotNone(alpine_container, "could not find alpine container")
+        self.assertIsNotNone(postgres_container, "could not find postgres container")
         self.assertIsNotNone(memcached_container, "could not find memcached container")
         self.assertIsNotNone(docker_container, "could not find docker container")
         
-        alpine_name = alpine_container.attrs["Name"]
+        postgres_name = postgres_container.attrs["Name"]
         memcached_name = memcached_container.attrs["Name"]
         docker_name = docker_container.attrs["Name"]
         
-        alpine_links = alpine_container.attrs["HostConfig"]["Links"]
+        postgres_links = postgres_container.attrs["HostConfig"]["Links"]
         memcached_links = memcached_container.attrs["HostConfig"]["Links"]
         docker_links = docker_container.attrs["HostConfig"]["Links"]
         
         self.assertIsNone(memcached_links)
         
-        expected_alpine_links = [
-            "{}:{}/{}".format(memcached_name, alpine_name, memcached_container.id)
+        expected_postgres_links = [
+            "{}:{}/{}".format(memcached_name, postgres_name, memcached_container.id)
         ]
-        self.assertEqual(expected_alpine_links, alpine_links)
+        self.assertEqual(expected_postgres_links, postgres_links)
         
         expected_docker_links = [
             "{}:{}/a-user-specified-alias".format(memcached_name, docker_name),
-            "{}:{}/{}".format(alpine_name, docker_name, alpine_container.id),
+            "{}:{}/{}".format(postgres_name, docker_name, postgres_container.id),
         ]
         self.assertEqual(expected_docker_links, docker_links)
