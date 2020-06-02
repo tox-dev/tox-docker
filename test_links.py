654599f0b4b5063e3d67d3096153c848afe2beb5
Gordon Pendleton <wgordonw1@gmail.com>
2020-06-02 11:59:51 -0400

    demonstrated multiple links work

--- test_links.py
+++ test_links.py
@@ -9,44 +9,67 @@ class ToxDockerLinksTest(unittest.TestCase):
 
     def test_links_created(self):
         client = docker.from_env(version="auto")
+        httpd_container = None
         registry_container = None
         nginx_container = None
         for container in client.containers.list():
+            if httpd_container is None and "httpd" in container.attrs['Config']['Image']:
+                httpd_container = container
+
             if registry_container is None and "registry" in container.attrs['Config']['Image']:
                 registry_container = container
             
             if nginx_container is None and "nginx" in container.attrs['Config']['Image']:
                 nginx_container = container
             
-            if all([registry_container, nginx_container]):
+            if all([httpd_container, registry_container, nginx_container]):
                 break
 
+        self.assertIsNotNone(httpd_container, "could not find httpd container")
         self.assertIsNotNone(registry_container, "could not find registry container")
         self.assertIsNotNone(nginx_container, "could not find nginx container")
         
+        httpd_name = httpd_container.attrs["Name"]
         registry_name = registry_container.attrs["Name"]
         nginx_name = nginx_container.attrs["Name"]
         
+        httpd_links = registry_container.attrs["HostConfig"]["Links"]
         registry_links = registry_container.attrs["HostConfig"]["Links"]
         nginx_links = nginx_container.attrs["HostConfig"]["Links"]
         
-        self.assertIsNone(registry_links)
+        self.assertIsNone(httpd_links)
+        
+        expected_registry_links = [
+            "{}:{}/{}".format(httpd_name, registry_name, 'httpd')
+        ]
+        self.assertEqual(expected_registry_links, registry_links)
         
         expected_nginx_links = [
-            "{}:{}/{}".format(registry_name, nginx_name, registry_container.id)
+            "{}:{}/{}".format(httpd_name, nginx_name, 'httpd')
+            "{}:{}/{}".format(registry_name, nginx_name, 'registry')
         ]
-        self.assertEqual(expected_nginx_links, nginx_links)
+        self.assertEqual(sorted(expected_nginx_links), sorted(nginx_links))
 
-    def test_registry_link_works(self):
+    def test_links_work(self):
         client = docker.from_env(version="auto")
+        registry_container = None
         nginx_container = None
         for container in client.containers.list():
-            if "nginx" in container.attrs['Config']['Image']:
+            if registry_container is None and "registry" in container.attrs['Config']['Image']:
+                registry_container = container
+
+            if nginx_container is None and "nginx" in container.attrs['Config']['Image']:
                 nginx_container = container
+            
+            if all([registry_container, nginx_container]):
                 break
+
+        self.assertIsNotNone(registry_container)
         self.assertIsNotNone(nginx_container)
-        exit_code = nginx_container.exec_run("curl --noproxy '*' http://registry:5000")[0]
-        self.assertEqual(exit_code, 0)
+        
+        self.assertEqual(registry_container.exec_run("ping -c 1 apache")[0], 0)
+        self.assertEqual(nginx_container.exec_run("curl --noproxy '*' http://hub:5000")[0], 0)
+        self.assertEqual(nginx_container.exec_run("curl --noproxy '*' http://apache")[0], 0)
 
     def test_validate_link_line_requires_alias(self):
         for line in (
