66681a9d68ea8f9b991bf9e83d9829cd1d74b892
Gordon Pendleton <wgordonw1@gmail.com>
2020-06-02 11:40:12 -0400

    added test for working link

--- test_links.py
+++ test_links.py
@@ -37,6 +37,17 @@ class ToxDockerLinksTest(unittest.TestCase):
         ]
         self.assertEqual(expected_nginx_links, nginx_links)
 
+    def test_registry_link_works(self):
+        client = docker.from_env(version="auto")
+        nginx_container = None
+        for container in client.containers.list():
+            if "nginx" in container.attrs['Config']['Image']:
+                nginx_container = container
+                break
+        self.assertIsNotNone(nginx_container)
+        exit_code = nginx_container.exec_run("curl --noproxy '*' http://registry:5000")[0]
+        self.assertEqual(exit_code, 0)
+
     def test_validate_link_line_requires_alias(self):
         for line in (
             'some-image-name',
