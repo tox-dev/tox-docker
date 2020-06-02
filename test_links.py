4d4f725828035ddd2475953c524d67ded98db7e7
Gordon Pendleton <wgordonw1@gmail.com>
2020-06-02 11:32:08 -0400

    adjusted test containers for required alias tests

--- test_links.py
+++ test_links.py
@@ -9,46 +9,33 @@ class ToxDockerLinksTest(unittest.TestCase):
 
     def test_links_created(self):
         client = docker.from_env(version="auto")
-        postgres_container = None
-        memcached_container = None
-        elasticsearch_container = None
+        registry_container = None
+        nginx_container = None
         for container in client.containers.list():
-            if postgres_container is None and "postgres" in container.attrs['Config']['Image']:
-                postgres_container = container
+            if registry_container is None and "registry" in container.attrs['Config']['Image']:
+                registry_container = container
             
-            if memcached_container is None and "memcached" in container.attrs['Config']['Image']:
-                memcached_container = container
+            if nginx_container is None and "nginx" in container.attrs['Config']['Image']:
+                nginx_container = container
             
-            if elasticsearch_container is None and "elasticsearch" in container.attrs['Config']['Image']:
-                elasticsearch_container = container
-            
-            if all([postgres_container, memcached_container, elasticsearch_container]):
+            if all([registry_container, nginx_container]):
                 break
 
-        self.assertIsNotNone(postgres_container, "could not find postgres container")
-        self.assertIsNotNone(memcached_container, "could not find memcached container")
-        self.assertIsNotNone(elasticsearch_container, "could not find elasticsearch container")
+        self.assertIsNotNone(registry_container, "could not find registry container")
+        self.assertIsNotNone(nginx_container, "could not find nginx container")
         
-        postgres_name = postgres_container.attrs["Name"]
-        memcached_name = memcached_container.attrs["Name"]
-        elasticsearch_name = elasticsearch_container.attrs["Name"]
+        registry_name = registry_container.attrs["Name"]
+        nginx_name = nginx_container.attrs["Name"]
         
-        postgres_links = postgres_container.attrs["HostConfig"]["Links"]
-        memcached_links = memcached_container.attrs["HostConfig"]["Links"]
-        elasticsearch_links = elasticsearch_container.attrs["HostConfig"]["Links"]
+        registry_links = registry_container.attrs["HostConfig"]["Links"]
+        nginx_links = nginx_container.attrs["HostConfig"]["Links"]
         
-        self.assertIsNone(memcached_links)
+        self.assertIsNone(registry_links)
         
-        expected_postgres_links = [
-            "{}:{}/{}".format(memcached_name, postgres_name, memcached_container.id)
+        expected_nginx_links = [
+            "{}:{}/{}".format(registry_name, nginx_name, registry_container.id)
         ]
-        self.assertEqual(expected_postgres_links, postgres_links)
-        
-        expected_elasticsearch_links = [
-            "{}:{}/a-user-specified-alias".format(memcached_name, elasticsearch_name),
-            "{}:{}/{}".format(postgres_name, elasticsearch_name, postgres_container.id),
-        ]
-        self.assertEqual(sorted(expected_elasticsearch_links), sorted(elasticsearch_links))
+        self.assertEqual(expected_nginx_links, nginx_links)
 
     def test_validate_link_line_requires_alias(self):
         for line in (
