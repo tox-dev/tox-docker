24a0a14d8f24bb8ee916b74be26268c6537ba189
Gordon Pendleton <wgordonw1@gmail.com>
2020-06-02 13:29:32 -0400

    verify the msg kwarg tests the exception message

--- test_links.py
+++ test_links.py
@@ -72,6 +72,5 @@ class ToxDockerLinksTest(unittest.TestCase):
         self.assertEqual(nginx_container.exec_run("curl --noproxy '*' http://httpd")[0], 0)
 
     def test_validate_link_line_rejects_dangling_comma(self):
-        with self.assertRaises(ValueError) as cm:
+        with self.assertRaises(ValueError, msg="FAIL Link specified against 'some-image-name' with dangling ':'. Remove it or add an alias."):
             _validate_link_line('some-image-name:')
-        self.assertEqual("Link specified against 'some-image-name' with dangling ':'. Remove it or add an alias.")
