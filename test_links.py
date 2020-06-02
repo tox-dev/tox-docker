11ddf06d3848abc20844f0c174c29dbf9d0351dd
Gordon Pendleton <wgordonw1@gmail.com>
2020-06-02 14:49:25 -0400

    Revert "added tests against elasticsearch to confirm slash in image name works"
    This reverts commit c29ce4fb06ed898b84b532e3ebae21fb7e5fe73e.

--- test_links.py
+++ test_links.py
@@ -10,14 +10,10 @@ class ToxDockerLinksTest(unittest.TestCase):
 
     def test_links_created(self):
         client = docker.from_env(version="auto")
-        es_container = None
         httpd_container = None
         registry_container = None
         nginx_container = None
         for container in client.containers.list():
-            if es_container is None and "elasticsearch" in container.attrs['Config']['Image']:
-                es_container = container
-                
             if httpd_container is None and "httpd" in container.attrs['Config']['Image']:
                 httpd_container = container
 
@@ -27,25 +23,21 @@ class ToxDockerLinksTest(unittest.TestCase):
             if nginx_container is None and "nginx" in container.attrs['Config']['Image']:
                 nginx_container = container
             
-            if all([es_container, httpd_container, registry_container, nginx_container]):
+            if all([httpd_container, registry_container, nginx_container]):
                 break
 
-        self.assertIsNotNone(es_container, "could not find elasticsearch container")
         self.assertIsNotNone(httpd_container, "could not find httpd container")
         self.assertIsNotNone(registry_container, "could not find registry container")
         self.assertIsNotNone(nginx_container, "could not find nginx container")
         
-        es_name = es_container.attrs["Name"]
         httpd_name = httpd_container.attrs["Name"]
         registry_name = registry_container.attrs["Name"]
         nginx_name = nginx_container.attrs["Name"]
         
-        es_links = es_container.attrs["HostConfig"]["Links"]
         httpd_links = httpd_container.attrs["HostConfig"]["Links"]
         registry_links = registry_container.attrs["HostConfig"]["Links"]
         nginx_links = nginx_container.attrs["HostConfig"]["Links"]
         
-        self.assertIsNone(es_links)
         self.assertIsNone(httpd_links)
         
         expected_registry_links = [
@@ -54,7 +46,6 @@ class ToxDockerLinksTest(unittest.TestCase):
         self.assertEqual(expected_registry_links, registry_links)
         
         expected_nginx_links = [
-            "{}:{}/es".format(es_name, nginx_name),
             "{}:{}/httpd".format(httpd_name, nginx_name),
             "{}:{}/hub".format(registry_name, nginx_name)
         ]
@@ -78,7 +69,6 @@ class ToxDockerLinksTest(unittest.TestCase):
         self.assertIsNotNone(nginx_container)
         
         self.assertEqual(registry_container.exec_run("ping -c 1 apache")[0], 0)
-        self.assertEqual(nginx_container.exec_run("curl --noproxy '*' http://es:9200")[0], 0)
         self.assertEqual(nginx_container.exec_run("curl --noproxy '*' http://hub:5000")[0], 0)
         self.assertEqual(nginx_container.exec_run("curl --noproxy '*' http://httpd")[0], 0)
 
