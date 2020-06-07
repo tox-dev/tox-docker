b292e10e8973ec39f86724e3c687b848e7201599
Dan Crosta <dcrosta@late.am>
2020-06-07 10:23:55 -0400

    move free-floading modules to tox_docker package

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         "docker>=2.3.0,<5.0",
         "tox>=2.7.0,<4.0",
     ],
-    py_modules=["tox_docker"],
+    packages=["tox_docker"],
     entry_points={"tox": ["docker = tox_docker"]},
     setup_requires=["vcversioner"],
     vcversioner={"version_module_paths" : ["_version.py"]},
