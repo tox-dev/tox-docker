311cf7386c1669f56c44e6ed98dbe350131994b9
Dan Crosta <dcrosta@late.am>
2020-07-30 15:05:18 -0400

    add basic pyroject.toml

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 
-
 setup(
     name="tox-docker",
     description="Launch a docker instance around test runs",
@@ -8,14 +7,10 @@ setup(
     url="https://github.com/tox-dev/tox-docker",
     maintainer="Dan Crosta",
     maintainer_email="dcrosta@late.am",
-    install_requires=[
-        "docker>=2.3.0,<5.0",
-        "tox>=2.7.0,<4.0",
-    ],
+    install_requires=["docker>=2.3.0,<5.0", "tox>=2.7.0,<4.0",],
     packages=["tox_docker"],
     entry_points={"tox": ["docker = tox_docker"]},
-    setup_requires=["vcversioner"],
-    vcversioner={"version_module_paths" : ["_version.py"]},
+    vcversioner={"version_module_paths": ["_version.py"]},
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Plugins",
