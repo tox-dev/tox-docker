85b1df721040ad971d4969d731c387acf7429207
Dan Crosta <dcrosta@late.am>
2021-09-12 10:02:52 -0400

    ci.sh use the right tox

--- ci.sh
+++ ci.sh
@@ -5,6 +5,15 @@ set -x
 tox_version=$1
 docker_version=$2
 
+if [ "$tox_version" -eq "tox-4.x" ]; then
+    tox=tox4
+    mypy_target=mypy-tox4
+else
+    tox=tox
+    mypy_target=mypy-tox3
+fi
+
+
 # our tox.ini sets up volumes within .tox, so make sure it exists
 # before trying to run tox...
 mkdir -p .tox
@@ -14,14 +23,10 @@ pip install --constraint $tox_version --constraint $docker_version .
 pip show tox tox-docker docker
 
 python tox_docker/tests/capture_containers_and_volumes.py .
-tox -e integration
+$tox -e integration
 python tox_docker/tests/assert_containers_and_volumes_unchanged.py .
 
-if [ "$tox_version" -eq "tox-4.x" ]; then
-    tox -e mypy-tox4
-else
-    tox -e mypy-tox3
-fi
+$tox -e $mypy_target
 
 echo "testing health check failure handling, an ERROR is expected:"
-tox -e healthcheck-failing 2>&1 | egrep "tox_docker.plugin.HealthCheckFailed: .* failed health check"
+$tox -e healthcheck-failing 2>&1 | egrep "tox_docker.plugin.HealthCheckFailed: .* failed health check"
