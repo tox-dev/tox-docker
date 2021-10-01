efab3fe161f1f6bf2a9baab0a707f579a34c20a5
Dan Crosta <dcrosta@late.am>
2021-10-01 15:11:48 -0400

    clean up logging mess

--- tox_docker/tox4/log.py
+++ tox_docker/tox4/log.py
@@ -1,28 +1,5 @@
-from logging import Logger
-from typing import Optional
-
-from tox.report import LOGGER, ToxHandler
-
-_HANDLER = None
-
-
-def _get_handler() -> ToxHandler:
-    global _HANDLER
-    if _HANDLER is None:
-        logger: Optional[Logger] = LOGGER
-        while logger:
-            for handler in logger.handlers:
-                if isinstance(handler, ToxHandler):
-                    _HANDLER = handler
-                    logger = None
-                    break
-            else:
-                logger = logger.parent if logger.propagate else None
-
-    assert _HANDLER is not None
-    return _HANDLER
+from logging import getLogger
 
 
 def log(line: str) -> None:
-    with _get_handler().with_context("docker"):
-        LOGGER.warning(line)
+    getLogger().warning(f"docker> {line}")
