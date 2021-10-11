5a11943502973412de0e51dda261fe0eafab2740
Gordon Pendleton <wgordonw1@gmail.com>
2021-10-11 14:20:15 -0400

    added container with custom command

--- tox_docker/tests/test_containers.py
+++ tox_docker/tests/test_containers.py
@@ -7,3 +7,11 @@ def test_it_can_run_two_of_the_same_image():
 
     assert hc_builtin.id != hc_custom.id
     assert hc_builtin.attrs["Image"] == hc_custom.attrs["Image"]
+
+
+def test_can_run_with_command():
+    default = find_container("networking-one")
+    custom = find_container("networking-cmd")
+
+    assert default.attrs["Args"] == ['/run.sh']
+    assert custom.attrs["Args"] == ['/run.sh', 'with', 'some', 'options']
