d28c081180b99c0cc2301ba4eddd6b0df9cecd98
Dan Crosta <dcrosta@late.am>
2022-01-22 10:36:58 -0500

    test & document that mapping a port to 0 assigns randomly
    fixes #116

--- tox_docker/tests/test_ports.py
+++ tox_docker/tests/test_ports.py
@@ -15,3 +15,9 @@ def test_it_exposes_only_specified_port() -> None:
     # this container remaps 1234/tcp to 2345, and hides 5678/udp
     assert os.environ["NETWORKING_TWO_1234_TCP_PORT"] == "2345"
     assert "NETWORKING_TWO_5678_UDP_PORT" not in os.environ
+
+
+def test_docker_picks_a_port_when_you_map_to_zero() -> None:
+    # tox.ini has `ports = 0:1234/tcp 0:5678/udp`
+    assert os.environ["NETWORKING_ONE_1234_TCP_PORT"] != "0"
+    assert os.environ["NETWORKING_ONE_5678_UDP_PORT"] != "0"
