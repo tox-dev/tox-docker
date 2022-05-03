50b7cd4a1590543011972e06dda67ede5f8bb035
Dan Crosta <dcrosta@late.am>
2022-05-03 13:13:14 -0400

    tox4 is now tox

--- ci.sh
+++ ci.sh
@@ -5,14 +5,6 @@ set -x
 tox_version=$1
 docker_version=$2
 
-if [ "$tox_version" = "tox-4.x" ]; then
-    tox=tox4
-    mypy_target=mypy-tox4
-else
-    tox=tox
-    mypy_target=mypy-tox3
-fi
-
 
 # our tox.ini sets up volumes within .tox, so make sure it exists
 # before trying to run tox...
@@ -23,10 +15,10 @@ pip install --constraint $tox_version --constraint $docker_version .
 pip show tox tox-docker docker
 
 python tox_docker/tests/capture_containers_and_volumes.py .
-$tox -e integration
+tox -e integration
 python tox_docker/tests/assert_containers_and_volumes_unchanged.py .
 
-$tox -e $mypy_target
+tox -e $mypy_target
 
 echo "testing health check failure handling, an ERROR is expected:"
-$tox -e healthcheck-failing 2>&1 | grep "'toxdocker/healthcheck' (from 'healthcheck-failing') failed health check"
+tox -e healthcheck-failing 2>&1 | grep "'toxdocker/healthcheck' (from 'healthcheck-failing') failed health check"
